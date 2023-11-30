02dfc0c10d26db3acfded309193532213160f277
s10260527 <s10260527@connect.np.edu.sg>
2023-11-30 19:11:56 +0800

    Blog Page

--- java/script.js
+++ java/script.js
@@ -1,23 +1,51 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const latestDescLinks = document.querySelectorAll('.latest-desc a');
-    latestDescLinks.forEach(link => {
-      link.addEventListener('mouseover', function() {
-        document.querySelector('.latest-post').classList.add('hover-effect');
-      });
-  
-      link.addEventListener('mouseout', function() {
-        document.querySelector('.latest-post').classList.remove('hover-effect');
-      });
-    });
-  
-    const overlayTxtLinks = document.querySelectorAll('.overlay-txt a');
-    overlayTxtLinks.forEach(link => {
-        link.addEventListener('mouseover', function() {
-            this.closest('.carousel-box').classList.add('hover-effect');
-        });
+// Elements with hover effect
 
-        link.addEventListener('mouseout', function() {
-            this.closest('.carousel-box').classList.remove('hover-effect');
-        });
-    });
+document.addEventListener('DOMContentLoaded', (event) => {
+  const overlayLinks = document.querySelectorAll('.latest-desc a');
+  overlayLinks.forEach(link => {
+      link.addEventListener('mouseover', function() {
+          this.closest('.latest-post').classList.add('hover-effect');
+      });
+
+      link.addEventListener('mouseout', function() {
+          this.closest('.latest-post').classList.remove('hover-effect');
+          this.closest('.latest-post').classList.add('original-position');
+      });
+  });
+
+  const carouselLinks = document.querySelectorAll('.overlay-txt a');
+  carouselLinks.forEach(link => {
+      link.addEventListener('mouseover', function() {
+          this.closest('.carousel-box').classList.add('hover-effect');
+      });
+
+      link.addEventListener('mouseout', function() {
+          this.closest('.carousel-box').classList.remove('hover-effect');
+          this.closest('.carousel-box').classList.add('original-position');
+      });
+  });
+
+  const featuredLinks = document.querySelectorAll('.featured-blog a');
+  featuredLinks.forEach(link => {
+      link.addEventListener('mouseover', function() {
+          this.closest('.featured-blog').classList.add('hover-effect');
+      });
+
+      link.addEventListener('mouseout', function() {
+          this.closest('.featured-blog').classList.remove('hover-effect');
+          this.closest('.featured-blog').classList.add('original-position');
+      });
+  });
+
+  const overlayTxtLinks = document.querySelectorAll('.blog-desc a');
+  overlayTxtLinks.forEach(link => {
+      link.addEventListener('mouseover', function() {
+          this.closest('.blog-box').classList.add('hover-effect');
+      });
+
+      link.addEventListener('mouseout', function() {
+          this.closest('.blog-box').classList.remove('hover-effect');
+          this.closest('.blog-box').classList.add('original-position');
+      });
+  })
 });
